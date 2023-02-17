5f8904a6c1b468c55c0da7282578c5b5d035f59f
KushalRaut <kushal.raut@kotuko.it>
2023-02-17 22:32:51 +0545

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import LoginButton from "./LoginButton";
+import { authOptions } from "pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth/next";
+
+const Navbar = async () => {
+  const session = await getServerSession(authOptions);
+  console.log(session);
+
+  return (
+    <>
+      <nav className="flex justify-between items-center py-8">
+        <Link href="/">
+          <h2 className="text-blue-600">Home</h2>
+        </Link>
+        <ul className="flex items-center gap-6">
+          <LoginButton />
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
